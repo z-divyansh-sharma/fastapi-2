a5d771fa2d61bd422cbbb181d14db29641edd94b
Dev4821 <divyansh.sharma0210@gmail.com>
2024-02-16 12:22:03 +0530

    implement middleware

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+import time
+
+from fastapi import FastAPI,Request
+from starlette.responses import JSONResponse
+
 from routers import users, admin
 from routers import tag, auth, todos
 from models import Base
@@ -9,8 +13,13 @@ app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
 
-
-
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 @app.get("/healthy")
 def health_check():
